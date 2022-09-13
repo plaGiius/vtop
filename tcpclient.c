36f20710e944170874ba1e098b92137dfc20ca8f
plaGiius <92948938+plaGiius@users.noreply.github.com>
2022-09-13 13:03:58 +0530

    Update tcpclient.c

--- tcpclient.c
+++ tcpclient.c
@@ -1,80 +1,57 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#define MAX 80
 
-int main()
-{ 
-    char *ip = "127.0.0.1";
-    int port = 5566;
-    int sock;
-    struct sockaddr_in addr;
-    socklen_t addr_size;
-    char buffer[1024];
-    int n;
-    char str[20];
-    int pnr;
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0){ 
-        perror("[-]Socket error");
-        exit(1);
-    }
-    memset(&addr, '\0', sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = port;
-    addr.sin_addr.s_addr = inet_addr(ip);
-    connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-    while(1){
-            bzero(buffer, 1024); 
-            recv(sock, buffer, sizeof(buffer), 0); // Boarding point 
-            printf("%s\n", buffer); 
-            
-            bzero(buffer, 1024);
-            scanf("%s", str);
-            strcpy(buffer, str);  
-            send(sock, buffer, strlen(buffer), 0); 
-            
-            bzero(buffer, 1024); 
-            recv(sock, buffer, sizeof(buffer), 0); // Dropping Point 
-            printf("%s\n", buffer); 
-            
-            bzero(buffer, 1024); 
-            scanf("%s", str);
-            strcpy(buffer, str);  
-            send(sock, buffer, strlen(buffer), 0); 
-        while (1){
-            bzero(buffer, 1024);
-            recv(sock, buffer, sizeof(buffer), 0); // Enter your name 
-            printf("%s\n", buffer); 
-            
-            bzero(buffer, 1024); 
-            scanf("%s", str);
-            strcpy(buffer, str); 
-            send(sock, buffer, strlen(buffer), 0);       
+void func(int sockfd);
+int main(void)
+{
+	int port = 6001;
+	char *ip = "127.0.0.1";
 
-            bzero(buffer, 1024); 
-            recv(sock, buffer, sizeof(buffer), 0); // Add passenger 
-            printf("%s\n", buffer); 
-            
-            bzero(buffer, 1024); 
-            scanf("%s", str);
-            strcpy(buffer, str); 
-            send(sock, buffer, strlen(buffer), 0); 
-            
-            if (strcmp(str,"n") == 0)
-                break;
-        }
-        recv(sock, &pnr, sizeof(pnr), 0);
-        printf("PNR Number: %d\n", pnr); 
-        
-        bzero(buffer, 1024); 
-        recv(sock, buffer, sizeof(buffer), 0); // Ticket Booked Successfully 
-        printf("%s\n", buffer); 
-
-        break;
-    }
-    close(sock); 
-    printf("Have a safe journey\n"); 
-    return 0; 
+	//declare socket
+	int server_id = socket(PF_INET,SOCK_STREAM,0);
+	//define the socket
+	struct sockaddr_in server;
+	server.sin_family = PF_INET;
+	server.sin_port = port;
+	server.sin_addr.s_addr = inet_addr(ip);
+	//try to establish communication
+	int connection_status = connect(server_id,(struct
+	sockaddr*)&server,sizeof(server));
+	if(connection_status == -1)
+	{
+		printf("connection error");
+		exit(9);
+	}
+	int num = 1;
+	func(server_id);
+	close(server_id);
+	}
+	void func(int sockfd)
+	{
+	char buff[MAX];
+	int n;
+	for (;;) {
+	bzero(buff, sizeof(buff));
+	printf("Enter the string : ");
+	n = 0;
+	while ((buff[n++] = getchar()) != '\n')
+	;
+	write(sockfd, buff, sizeof(buff));
+	bzero(buff, sizeof(buff));
+	read(sockfd, buff, sizeof(buff));
+	printf("From Server : %s", buff);
+	if ((strncmp(buff, "exit", 4)) == 0) 
+	{
+		printf("Client Exit...\n");
+		break;
+	}
+	}
 }
